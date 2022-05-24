76c971946c916fbd0364b3ddd893465e10c4df33
Lakeitra Coleman <colemantakiya@gmail.com>
2022-05-24 16:50:02 -0500

    initial commit

---
+++ ClassExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2015;
+
+            var chevy = new Car()
+            {
+                Make = "Chevy",
+                Model = "Impala",
+                Year = 2016
+            };
+
+            var dodge = new Car("Dodge", "Ram", 2003);
+
+            var carList = new List<Car>() { myCar, chevy, dodge };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+        }
+    }
+}
